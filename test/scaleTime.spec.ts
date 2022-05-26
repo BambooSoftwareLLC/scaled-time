e347c0c9074d41a4272255ef8e9927dd9866acad
michael@mharris.io <19492392+Burwin@users.noreply.github.com>
2022-05-26 17:12:06 -0400

    scales for Date.now()

--- test/scaleTime.spec.ts
+++ test/scaleTime.spec.ts
@@ -1,9 +1,9 @@
-import { realTime, resetTime, scaleTime } from "../src";
+import { resetTime, scaleTime } from "../src";
 
 describe("scaleTime", () => {
   afterEach(() => resetTime());
 
-  it("should scale properly", async () => {
+  it("should scale properly calling new Date()", async () => {
     const scalingFactor = 2;
     const start = new Date().getTime();
 
@@ -17,6 +17,33 @@ describe("scaleTime", () => {
 
     const scaledTime = scaledDate.getTime();
 
+    // compare scaled to actual
+    resetTime();
+    const actualTime = new Date().getTime();
+
+    expect(actualTime).toBeLessThan(scaledTime);
+
+    const actualDiff = actualTime - start;
+    const scaledDiff = scaledTime - start;
+
+    expect(actualDiff).toBeLessThan(scaledDiff)
+
+    const scale = scaledDiff / actualDiff;
+    expect(scale).toBeCloseTo(2, 1);
+  });
+
+  it("should scale properly calling Date.now()", async () => {
+    const scalingFactor = 2;
+    const start = Date.now();
+
+    scaleTime(scalingFactor);
+
+    let scaledTime = Date.now();
+    for (let i = 0; i < 10000; i++) {
+      // do something that takes non-zero time
+      scaledTime = Date.now();
+    }
+    
     // compare scaled to actual
     resetTime();
     const actualTime = new Date().getTime();
